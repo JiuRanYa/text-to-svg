0397f35deeabad5f7b097227ce0931f9e19d4c97
JiuRanYa <1084350607@qq.com>
2025-06-11 10:36:06 +0800

    feat: Remove unused imports from layout and Header components to clean up code

--- app/_component/Header.tsx
+++ app/_component/Header.tsx
@@ -2,7 +2,6 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { cn } from '@/core/lib/utils'
-import Image from 'next/image'
 
 const navigation = [
   { name: 'Home', href: '/' },
